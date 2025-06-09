582f5943e340adefc6b7cd107d8780330a5fa599
Toridesu <160250540+Toridesu@users.noreply.github.com>
2025-06-09 18:33:38 +0900

    GitHub Pagesのデプロイ設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? "/knowledge-ec" : "",
   /* config options here */
   images: {
     remotePatterns: [
@@ -13,6 +17,7 @@ const nextConfig: NextConfig = {
         hostname: "avatars.githubusercontent.com",
       },
     ],
+    unoptimized: true,
   },
 };
 
